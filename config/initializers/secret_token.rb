d261d2fd39eed0badd83f782caaaf209add02d7f
Ben Askins <ben.askins@gmail.com>
2013-11-19 18:39:48 +1100

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StuffFinder::Application.config.secret_key_base = 'a6ecea871963b24d4790e67e1adf919a130b931f241e79675ada353a56a072b24019f497f1e1757dd289880597d47cb06ecf49cdabab32c4b639c0b448f92054'
