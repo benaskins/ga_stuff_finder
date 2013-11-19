2e4b4b1684c910f8de7c210298049049bb8201bd
Ben Askins <ben.askins@gmail.com>
2013-11-19 19:04:33 +1100

    Seed categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+["Personals", "For Sale", "Garage Sales", "Entertainment"].each do |category|
+  Category.create(:name => category) unless Category.exists?(:name => category)
+end
