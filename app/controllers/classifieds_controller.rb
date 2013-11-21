79a63b89aa1bf2b87080d88d49fb09bd35ea2d5e
Ben Askins <ben.askins@gmail.com>
2013-11-21 23:20:28 +1100

    Add simple_form, and an example form

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -1,5 +1,12 @@
 class ClassifiedsController < ApplicationController
+
+  def new
+    @classified = Classified.new
+  end
+
+
   def show
     @classified = Classified.find(params[:id])
   end
+
 end
